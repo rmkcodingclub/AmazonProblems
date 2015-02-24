c3dfef878115b6ab5e18e1f4925d9a0010d8a90e
RMK Coding Club <csecodingclub@gmail.com>
2015-02-24 20:20:44 +0530

    Create problem38.c

---
+++ problem38.c
@@ -0,0 +1,10 @@
+The answer for Qn. No.38 is
+
+Option B 
+
+since the expression given below
+{
+return ((p==null)||(p->next==null)|| (p->info<=p->next->info)&&( fun(p->next)));
+}
+
+will be evaluated to true when the list is empty or it has one node or arranged in ascending order.
