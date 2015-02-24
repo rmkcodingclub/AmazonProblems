c2876ec4d5f5f6964eeae5f87312534b9c3c9869
RMK Coding Club <csecodingclub@gmail.com>
2015-02-24 21:22:31 +0530

    Create prob9.c

---
+++ prob9.c
@@ -0,0 +1,15 @@
+//Solved by Divija, Dept of CSE
+
+
+#include<stdio.h>
+#include<stdlib.h>
+main()
+int i,j,n,k;
+int input;
+printf("enter the size of the array");
+scanf("%d",&n);
+input=(int*)
+malloc(size of (int)*(n+1));
+for(i=1;i<=n;i++)
+printf("enter the value should be between 1 and %d",n)
+scanf("%d",&input[i]);
