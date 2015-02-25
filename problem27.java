82808fae9dbacec79c13d5a16b47b25541afa155
RMK Coding Club <csecodingclub@gmail.com>
2015-02-25 20:27:26 +0530

    Create problem27.java

---
+++ problem27.java
@@ -0,0 +1,7 @@
+/**
+By Lakshmi Narayana V, EIE
+*/
+
+public class notpresent {
+
+/** * @param args */public static void main(String[] args) { Scanner s=new Scanner(System.in); int n=s.nextInt(); int arr[]=new int[n]; for(int i=0;i<n;i++) arr[s.nextInt()-1]=1; for(int i=0;i<n;i++) if(arr[i]==0) System.out.println(i+1);}}
