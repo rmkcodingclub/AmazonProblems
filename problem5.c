ea7678e092ed01acae69e08067ded74171eec2b1
RMK Coding Club <csecodingclub@gmail.com>
2015-02-25 20:05:39 +0530

    Create problem5.c

---
+++ problem5.c
@@ -0,0 +1,31 @@
+//Solved by S.Balakumar, CSE
+
+#include<stdio.h>
+
+int FindMaxSum(int arr[], int n)
+{
+  int a = arr[0];
+  int b = 0;
+  int c;
+  int i;
+ 
+  for (i = 1; i < n; i++)
+  {
+    
+     c = (a> b)?a: b;
+     a = b + arr[i];
+     b = c;
+  }
+ 
+ 
+   return ((a> b)? a : b);
+}
+ 
+
+int main()
+{
+  int arr[] = {5, 5, 10, 100, 10, 5};
+  printf("%d \n", FindMaxSum(arr, 6));
+  getchar();
+  return 0;
+}
