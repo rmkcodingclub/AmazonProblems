1396ec0a1ed7d80b9bbb5018568d6bb2d0926119
RMK Coding Club <csecodingclub@gmail.com>
2015-02-25 19:50:08 +0530

    Create problem19.c

---
+++ problem19.c
@@ -0,0 +1,41 @@
+/* SOlved by Priya D, CSE */
+
+#include<stdio.h>
+
+#include<conio.h>
+
+void main()
+
+{
+
+float  a[20],xor,c;
+
+int i,n,repeat;
+
+clrscr();
+
+printf(“To find the Reapeated number “);
+
+printf(“Enter the value of Array”);
+
+scanf(“%d”,&n);
+
+for(i=0;i<=(n-1);i++)
+
+{
+
+Scanf(“%f”,&a[i]);
+
+mul=a[i]*100;
+
+}
+
+Xor=mul/10;
+
+C=xor/10;
+
+printf(“\n The repeated num is %f\n”,c);
+
+getch();
+
+}
