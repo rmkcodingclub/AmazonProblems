6907df88bdca8c30a274c3ba3f5c1bfa00f57b32
RMK Coding Club <csecodingclub@gmail.com>
2015-02-25 20:07:35 +0530

    Create problem25.c

---
+++ problem25.c
@@ -0,0 +1,37 @@
+//By T.Poovarasan , ECE
+
+#include<stdio.h>
+ 
+/*Function to return max sum such that no two elements
+ are adjacent */
+int FindMaxSum(int arr[], int n)
+{
+  int incl = arr[0];
+  int excl = 0;
+  int excl_new;
+  int i;
+ 
+  for (i = 1; i < n; i++)
+  {
+     /* current max excluding i */
+     excl_new = (incl > excl)? incl: excl;
+ 
+     /* current max including i */
+     incl = excl + arr[i];
+     excl = excl_new;
+  }
+ 
+   /* return max of incl and excl */
+   return ((incl > excl)? incl : excl);
+}
+ 
+/* Driver program to test above function */
+int main()
+{
+  int arr[] = {5, 5, 10, 100, 10, 5};
+  printf("%d \n", FindMaxSum(arr, 6));
+  getchar();
+  return 0;
+}
+//Time Complexity: O(n)
+
